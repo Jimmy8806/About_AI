594adf4c30603963350babf1bc09798d58919403
Ahmed Jamal <126389512+Jimmy8806@users.noreply.github.com>
2025-12-21 16:10:52 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const yearEl = document.getElementById("year");
+
+if (yearEl) {
+    yearEl.textContent = `© ${new Date().getFullYear()} — Eng: ALi Ahmed Mohammed && Eng : Ahmed Jamal Abdelmoniem && Eng : Abdulrahman Ashraf Mohammed && Eng : Abdullah Talaat Hassan`;
+}
